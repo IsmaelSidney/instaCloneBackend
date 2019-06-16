a91dc52f13f82deb86d42c546e72fbf86bbd9cd1
ismael sidney <ismaelsidney@gmail.com>
2019-06-16 20:10:01 -0300

    connect mongo

--- src/index.js
+++ src/index.js
@@ -6,10 +6,19 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0-yfmw4.mongodb.net/test?retryWrites=true&w=majority',{
+io.on('connection', socket =>{
+    socket.on('connectRoom', posts => {
+        socket.join(posts);
+    })
+})
+mongoose.connect(
+    'mongodb+srv://admin:admin@cluster0-yfmw4.mongodb.net/test?retryWrites=true&w=majority',
+{
     useNewUrlParser: true,
 });
 
@@ -19,10 +28,8 @@ app.use((req,res, next)=>{
     next();
 })
 
-app.use(cors());
-
 app.use('/files', express.static(path.resolve(__dirname,'..','uploads','resized')));
-
+app.use(express.urlencoded({extended : true}));
 app.use(require('./routes'));
 
 server.listen( process.env.PORT || 3000);
