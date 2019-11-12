10d6b47ba78609bdc69722eef9eab03912fac26b
ismael <mael-sf@hotmail.com>
2019-11-12 17:08:36 -0300

    update

--- src/index.js
+++ src/index.js
@@ -12,9 +12,8 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 mongoose.connect(
-    'mongodb+srv://admin:admin@cluster0-yfmw4.mongodb.net/test?retryWrites=true&w=majority',
-{
-    useNewUrlParser: true,
+    'mongodb+srv://omnistack:omnistack@cluster0-yfmw4.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+    useNewUrlParser: true
 });
 
 app.use((req,res, next)=>{
@@ -26,4 +25,4 @@ app.use((req,res, next)=>{
 app.use('/files', express.static(path.resolve(__dirname,'..','uploads','resized')));
 app.use(require('./routes'));
 
-server.listen( process.env.PORT || 4444);
+server.listen( process.env.PORT || 3333);
